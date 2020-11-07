a56e630475bc9a540ff861713b7a09c8e81ec965
borzor <64104945+borzor@users.noreply.github.com>
2020-11-07 21:33:25 +0300

    Update class.cpp

--- serialization/class.cpp
+++ serialization/class.cpp
@@ -8,7 +8,7 @@ test::test(std::string_view path, std::size_t block_size,int flags)
     : block_size(block_size),vec(block_size)
     {
         if((fd_ = open(std::string(path).c_str(), flags))==-1)
-            throw std::system_error(errno,std::system_category(),"shit");
+            throw std::system_error(errno,std::system_category(),"open failed");
 }
 int test::random(int min, int max) {
     std::mt19937 prng(std::random_device{}());
@@ -20,7 +20,7 @@ void test::test_test()
     offset*=block_size;
     uint64_t ret = pread(fd_, &vec[0], block_size, offset);
     if(ret!=vec.size())
-        throw std::system_error(errno,std::system_category(),"shit x2");
+        throw std::system_error(errno,std::system_category(),"read failed");
 }
 test::~test()
 {
